aa6432d3bbea9f48f3f45be48997a2b7079cc528
Sergii Kabashniuk <skabashniuk@redhat.com>
2019-04-04 10:56:21 +0300

    Reworked configuration

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/eclipse/che-go-jsonrpc"
 	"github.com/eclipse/che-go-jsonrpc/jsonrpcws"
 	"github.com/eclipse/che-plugin-broker/model"
 	"github.com/gorilla/websocket"
+	"github.com/kelseyhightower/envconfig"
 	"log"
 	"os"
-	"sync"
+	"os/signal"
+	"strconv"
+	"strings"
 	"time"
 )
+
+var Suspending bool = false
+
+type Configuration struct {
+	CheHost      string        `required:"true" split_words:"true"`
+	CheToken     string        `split_words:"true"`
+	MajorThreads int           `default:"10" split_words:"true"`
+	MinorThreads int           `default:"10" split_words:"true"`
+	WsTimeout    time.Duration `default:"10s" split_words:"true"`
+	Secure       bool          `default:"false" split_words:"true"`
+}
 
 func ConnectOrFail(endpoint string, token string) *jsonrpc.Tunnel {
 	tunnel, err := Connect(endpoint, token)
@@ -28,52 +41,77 @@ func Connect(endpoint string, token string) (*jsonrpc.Tunnel, error) {
 	return jsonrpc.NewManagedTunnel(conn), nil
 }
 
+func SendMessagesInLoop(wsUrl, token, senderId string) {
+
+	tunnel := ConnectOrFail(wsUrl, token)
+	defer tunnel.Close()
+
+	tunnel.Conn()
+
+	for !Suspending {
+		message := fmt.Sprintf("Message sent from %s", senderId)
+		event := &model.PluginBrokerLogEvent{
+			RuntimeID: model.RuntimeID{Workspace: "ws1", Environment: "e1", OwnerId: "own1"},
+			Text:      message,
+			Time:      time.Now(),
+		}
+		//log.Print(message)
+		if err := tunnel.Notify(event.Type(), event); err != nil {
+			log.Fatalf("Trying to send event of type '%s' to closed tunnel '%s'", event.Type(), tunnel.ID())
+		}
+	}
+	fmt.Printf("Sending complete %s\n", senderId)
+}
+
 func main() {
 	log.SetOutput(os.Stdout)
-	var cheUrl string
-	flag.StringVar(&cheUrl, "cheurl", "ws://che-eclipse-che.192.168.64.12.nip.io/api/websocket", "Che endpoint url")
-	var cheToken string
-	flag.StringVar(&cheToken, "token", "", "Che token")
 
-	numOfThreads := flag.Int("tnum", 100, "number of threads sending messages")
-	numOfMessages := flag.Int("mnum", 10000, "number of messages to send")
-	wsTimeout := flag.Int("wstimeout", 10, "websocket connection timeout seconds")
-	flag.Parse()
-
-	fmt.Println("thum:", *numOfThreads)
-	fmt.Println("mnum:", *numOfMessages)
-	fmt.Println("cheurl:", cheUrl)
-	fmt.Println("cheToken:", cheToken)
-	fmt.Println("wstimeout:", *wsTimeout)
-
-	websocket.DefaultDialer.HandshakeTimeout = time.Duration(*wsTimeout) * time.Second
-
-	wg := &sync.WaitGroup{}
-	wg.Add(*numOfThreads)
-
-	for i := 0; i < *numOfThreads; i++ {
-		go func(endpoint string, token string, th int) {
-			tunnel := ConnectOrFail(endpoint, token)
-			defer tunnel.Close()
-
-			tunnel.Conn()
-
-			for j := 0; j < *numOfMessages; j++ {
-				message := fmt.Sprintf("Messaget from thread %d number %d", th, j)
-				event := &model.PluginBrokerLogEvent{
-					RuntimeID: model.RuntimeID{Workspace: "ws1", Environment: "e1", OwnerId: "own1"},
-					Text:      message,
-					Time:      time.Now(),
-				}
-				log.Print(message)
-				if err := tunnel.Notify(event.Type(), event); err != nil {
-					log.Fatalf("Trying to send event of type '%s' to closed tunnel '%s'", event.Type(), tunnel.ID())
-				}
-			}
-
-			wg.Done()
-
-		}(cheUrl, cheToken, i)
+	var configuration Configuration
+	err := envconfig.Process("JsonRpcLoader", &configuration)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
-	wg.Wait()
+
+	format := "CheHost: %s\nToken: %s\nMajorThreads: %d\nMinorThreads: %d\nTimeout: %s\nSecure: %v\n"
+	_, err = fmt.Printf(format, configuration.CheHost, configuration.CheToken, configuration.MajorThreads, configuration.MinorThreads, configuration.WsTimeout, configuration.Secure)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	websocket.DefaultDialer.HandshakeTimeout = configuration.WsTimeout
+
+	var major, minor strings.Builder
+	if configuration.Secure {
+		major.WriteString("wss://")
+		minor.WriteString("wss://")
+	} else {
+		major.WriteString("ws://")
+		minor.WriteString("ws://")
+	}
+	major.WriteString(configuration.CheHost)
+	major.WriteString("/api/websocket")
+	minor.WriteString(configuration.CheHost)
+	minor.WriteString("/api/websocket-minor")
+
+	for i := 0; i < configuration.MajorThreads; i++ {
+		go SendMessagesInLoop(major.String(), configuration.CheToken, "major"+ strconv.Itoa(i))
+	}
+
+	for i := 0; i < configuration.MajorThreads; i++ {
+		go SendMessagesInLoop(minor.String(), configuration.CheToken, "minor"+ strconv.Itoa(i))
+	}
+	// After setting everything up!
+	// Wait for a SIGINT (perhaps triggered by user with CTRL-C)
+	// Run cleanup when signal is received
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan struct{})
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		<-signalChan
+		fmt.Println("\nReceived an interrupt, stopping services...")
+		Suspending = true
+		time.Sleep(5 * time.Second)
+		close(cleanupDone)
+	}()
+	<-cleanupDone
 }
